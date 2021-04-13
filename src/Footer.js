e47fb2e0d56d32699ee0d7f4cba70680c2805ff5
Gagandeep Singh <gagandeep349000@gmail.com>
2021-04-13 15:57:22 +1200

    changed class attribute to className

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,15 @@ class Footer extends Component
     render()
     {
         return(
-            <footer class="card text-white bg-dark mb-0">
-            <div class="container">
-              <p class="float-end mb-1">
+            <footer className="card text-white bg-dark mb-0">
+            <div className="container">
+              <p className="float-end mb-1">
                 <a href="Button">Back to top</a>
               </p>
-              <p class="mb-1">Name-Gagandeep Singh </p>
-              <p class="mb-0">Student ID-30029497
+              <p className="mb-1">Name-Gagandeep Singh </p>
+              <p className="mb-0">Student ID-30029497
               </p>
-              <p class="mb-0">Subject number - 6210
+              <p className="mb-0">Subject number - 6210
               </p>
             
                 </div>
