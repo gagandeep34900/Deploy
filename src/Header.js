df51a394aae1d26143150af4f0e6b299e494e12b
Gagandeep Singh <gagandeep349000@gmail.com>
2021-04-13 15:50:14 +1200

    removed anchor tag in Header.js

--- src/Header.js
+++ src/Header.js
@@ -31,12 +31,12 @@ class Header extends Component
 </div>
 <div class="navbar navbar-dark bg-dark shadow-sm">
 <div class="container">
-<a href="g" class="navbar-brand d-flex align-items-center">
+<p class="navbar-brand d-flex align-items-center">
   <svg class="svg-icon" viewBox="0 0 20 20">
       <path d="M14.911,1.295H5.09c-0.737,0-1.339,0.603-1.339,1.339v14.733c0,0.736,0.603,1.338,1.339,1.338h9.821c0.737,0,1.339-0.602,1.339-1.338V2.634C16.25,1.898,15.648,1.295,14.911,1.295 M15.357,17.367c0,0.24-0.205,0.445-0.446,0.445H5.09c-0.241,0-0.446-0.205-0.446-0.445v-0.893h10.714V17.367z M15.357,15.58H4.644V4.42h10.714V15.58z M15.357,3.527H4.644V2.634c0-0.241,0.205-0.446,0.446-0.446h9.821c0.241,0,0.446,0.206,0.446,0.446V3.527z"></path>
   </svg>
   <strong>Assginment 1 React-6210</strong>
-</a>
+</p>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
